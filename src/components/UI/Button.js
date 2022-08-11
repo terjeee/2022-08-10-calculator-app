5efa2863f9967c40489ef66c29fcc578a5fe8b66
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-11 15:32:10 +0200

    endelig funker fargene på knappene

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,9 +2,8 @@ import css from './Button.module.css';
 
 function Button(props) {
   const btnColor = props.color;
-  console.log(btnColor, typeof btnColor);
 
-  return <button className={`${css.button} ${css.btnColor}`}>{props.children}</button>;
+  return <button className={`${css.button} ${css[`${btnColor}`]}`}>{props.children}</button>;
 }
 
 export default Button;
