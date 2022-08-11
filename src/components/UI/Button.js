f3fd79907f096cbd0101e52fbff99b8ee109e3a8
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-11 16:07:27 +0200

    idk

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -2,8 +2,13 @@ import css from './Button.module.css';
 
 function Button(props) {
   const btnColor = props.color;
+  const btnValue = props.value;
 
-  return <button className={`${css.button} ${css[btnColor]}`}>{props.children}</button>;
+  return (
+    <button className={`${css.button} ${css[btnColor]}`} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
