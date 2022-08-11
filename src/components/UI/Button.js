804e8637398533953d3f84954505d755118f61f0
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-11 15:37:59 +0200

    fjernet unødvendig `${}`

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -3,7 +3,7 @@ import css from './Button.module.css';
 function Button(props) {
   const btnColor = props.color;
 
-  return <button className={`${css.button} ${css[`${btnColor}`]}`}>{props.children}</button>;
+  return <button className={`${css.button} ${css[btnColor]}`}>{props.children}</button>;
 }
 
 export default Button;
