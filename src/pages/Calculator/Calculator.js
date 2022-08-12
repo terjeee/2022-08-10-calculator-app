55cb45eb5c481e20d2e6fe36230ccd3874efde53
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-12 02:00:49 +0200

    idk

--- src/pages/Calculator/Calculator.js
+++ src/pages/Calculator/Calculator.js
@@ -5,6 +5,10 @@ import css from './Calculator.module.css';
 function Calculator() {
   let sum = 0;
 
+  const handleGetValue = (event) => {
+    console.log(event.target.innerText);
+  };
+
   return (
     <main className={css.calc__main}>
       <div className={css.calc__sum}>
@@ -12,27 +16,35 @@ function Calculator() {
       </div>
       <div className={css.calc__buttons}>
         <div className={css.grid}>
-          <Button>7</Button>
-          <Button>8</Button>
-          <Button>9</Button>
-          <Button color='blue'>DEL</Button>
-          <Button>4</Button>
-          <Button>5</Button>
-          <Button>6</Button>
-          <Button>+</Button>
-          <Button>1</Button>
-          <Button>2</Button>
-          <Button>3</Button>
-          <Button>-</Button>
-          <Button>.</Button>
-          <Button>0</Button>
-          <Button>/</Button>
-          <Button>x</Button>
+          <Button onClick={handleGetValue} value='7'>
+            7
+          </Button>
+          <Button onClick={handleGetValue}>8</Button>
+          <Button onClick={handleGetValue}>9</Button>
+          <Button onClick={handleGetValue} color='blue'>
+            DEL
+          </Button>
+          <Button onClick={handleGetValue}>4</Button>
+          <Button onClick={handleGetValue}>5</Button>
+          <Button onClick={handleGetValue}>6</Button>
+          <Button onClick={handleGetValue}>+</Button>
+          <Button onClick={handleGetValue}>1</Button>
+          <Button onClick={handleGetValue}>2</Button>
+          <Button onClick={handleGetValue}>3</Button>
+          <Button onClick={handleGetValue}>-</Button>
+          <Button onClick={handleGetValue}>.</Button>
+          <Button onClick={handleGetValue}>0</Button>
+          <Button onClick={handleGetValue}>/</Button>
+          <Button onClick={handleGetValue}>x</Button>
           <div className={css['grid__item--span02']}>
-            <Button color='blue'>RESET</Button>
+            <Button onClick={handleGetValue} color='blue'>
+              RESET
+            </Button>
           </div>
           <div className={css['grid__item--span02']}>
-            <Button color='red'>=</Button>
+            <Button onClick={handleGetValue} color='red'>
+              =
+            </Button>
           </div>
         </div>
       </div>
