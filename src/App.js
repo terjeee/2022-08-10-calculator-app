ae1b2e8a70973ee931a5daccf930e7adc611e011
terjeee <96795863+terjeee@users.noreply.github.com>
2022-08-14 04:30:41 +0200

    :)

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import Calculator from './pages/Calculator/Calculator';
 
 function App() {
-  return <Calculator />;
+  return (
+    <>
+      <Calculator />
+      {/* <footer>
+        <div>
+          Challenge by
+          <a href='https://www.frontendmentor.io/challenges/advice-generator-app-QdUG-13db'>
+            Frontend Mentor
+          </a>
+          Coded by
+          <a href='https://github.com/terjeee'>terjeee</a>
+        </div>
+      </footer> */}
+    </>
+  );
 }
 
 export default App;
